95c28dc77c9fdc2b4d2c90684a215833e65d712e
marco7877 <marcoantonio7877@gmail.com>
2024-11-20 11:43:39 +0100

    change name

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""package-template setup script"""
+"""ME-PCA setup script"""
 from setuptools import setup
 
 import versioneer
 
 if __name__ == "__main__":
     setup(
-        name="package-template",
+        name="ME-PCA",
         version=versioneer.get_version(),
         cmdclass=versioneer.get_cmdclass(),
         zip_safe=False,
